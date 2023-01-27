87aa5f56f35a9b9c1b4775f0c5e8d9d25edd56f7
James <james.john.wood97@gmail.com>
2023-01-27 13:21:33 +0000

    Fixed bug in PaintReader class preventing csv files from being read correctly

--- paint_reader.py
+++ paint_reader.py
@@ -3,16 +3,16 @@ import paint
 
 
 class PaintReader:
-    def __init__(self, paint_file_name = "paints.csv"):
+    def __init__(self, paint_file_name: str = "paints.csv"):
         self.paint_file_name = paint_file_name
 
-    def read_paint_files(self, paint_file_name: str | None) -> list:
+    def read_paint_files(self, paint_file_name: str | None = None) -> list:
         paints = []
         if paint_file_name is None:
             paint_file_name = self.paint_file_name
         with open(paint_file_name) as file:
-            csvFile = csv.DictReader(file)
-            for line in csvFile:
+            csv_file = csv.reader(file)
+            for line in csv_file:
                 paint_name = line[0]
                 paint_cost_per_litre = float(line[1])
                 paints.append(paint.Paint(paint_name, paint_cost_per_litre))
